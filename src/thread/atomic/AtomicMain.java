07eb94042d162be74adb64837d4134488e97d9f6
zhangqi61 <zhangqi61@baidu.com>
2021-12-14 10:53:34 +0800

    【update】

--- src/thread/atomic/AtomicMain.java
+++ src/thread/atomic/AtomicMain.java
@@ -1,5 +1,7 @@
 package thread.atomic;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * Atomic Main
  */
@@ -26,5 +28,19 @@ public class AtomicMain {
 
         System.out.println("value == " + value);
         System.out.println("fieldUpdaterValue == " + fieldUpdaterValue);
+
+        User user1 = new User("张三", 1);
+        User user2 = new User("李四", 2);
+        User user3 = new User("王五", 3);
+
+        // AtomicReference 是对普通对象的封装
+        AtomicReference<User> userAtomicReference = new AtomicReference<>();
+        userAtomicReference.set(user1);
+
+        userAtomicReference.compareAndExchange(user1, user2);
+        System.out.println("current user is " + userAtomicReference.get());
+
+        userAtomicReference.compareAndExchange(user1, user3);
+        System.out.println("current user is " + userAtomicReference.get());
     }
 }
