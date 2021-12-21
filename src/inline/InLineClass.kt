8796adb596add6104ab03538a9cd89644d26630b
zhangqi61 <zhangqi61@baidu.com>
2021-12-21 15:07:25 +0800

    [update]

---
+++ src/inline/InLineClass.kt
@@ -0,0 +1,12 @@
+package inline
+
+class InLineClass {
+
+    inline fun request(type: Int, call: (code: Int, type: Int) -> Unit) {
+        call.invoke(1, 2)
+    }
+
+    fun testArray() {
+
+    }
+}
