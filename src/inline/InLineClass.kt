c7c44e8cec849d73d78182d3292af049d9ee1660
zhangqi61 <zhangqi61@baidu.com>
2022-02-25 22:07:20 +0800

    【update】

--- src/inline/InLineClass.kt
+++ src/inline/InLineClass.kt
@@ -2,9 +2,9 @@ package inline
 
 class InLineClass {
 
-    inline fun request(type: Int, call: (code: Int, type: Int) -> Unit) {
-        call.invoke(1, 2)
-    }
+//    inline fun request(type: Int, call: (code: Int, type: Int) -> Unit) {
+//        call.invoke(1, 2)
+//    }
 
     fun testArray() {
 
--- src/string/StringMain.java
+++ src/string/StringMain.java
@@ -8,5 +8,9 @@ public class StringMain {
         boolean result = oldName.toLowerCase().equals(newName.toLowerCase());
 
         boolean result2 = oldName.toUpperCase().equals(newName.toUpperCase());
+
+        if (null instanceof String){
+            System.out.println("hahahah");
+        }
     }
 }
