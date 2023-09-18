b158357c6458eaa4680cf931484b6a80792d6444
zhangqi61 <zhangqi61@baidu.com>
2023-09-18 23:41:09 +0800

    【Java】【tree】二叉堆

---
+++ src/base/arithmetic/soort/DQSort.java
@@ -0,0 +1,41 @@
+package base.arithmetic.soort;
+
+/**
+ * 排序
+ */
+public class DQSort {
+
+    /**
+     * 冒泡排序
+     *
+     * @param array 数组
+     */
+    public void bubbleSort(int array[]) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+        int len = array.length;
+        int lastExchangeIndex = 0;
+        int sortBorder = len - 1;
+        // 外层循环控制回合
+        for (int i = 0; i < len - 1; i++) {
+            // 每一轮的标记
+            boolean isSorted = true;
+            // 内层循环控制每轮的交换
+            for (int j = 0; j < sortBorder; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    isSorted = false;
+                    lastExchangeIndex = j;
+                }
+            }
+            sortBorder = lastExchangeIndex;
+            // 提前结束循环，避免重复循环
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+}
